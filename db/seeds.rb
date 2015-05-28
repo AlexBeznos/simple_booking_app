5a67f190a4a396a219d84ebb55c3dbf1cafa6c12
Alex Beznos <beznosa@yahoo.com>
2015-05-28 18:50:18 +0300

    seed refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,13 @@
 Reservation.create!([
   {start_time: "2015-05-28 19:00:00", end_time: "2015-05-28 20:00:00", table: 1},
   {start_time: "2015-05-28 20:30:00", end_time: "2015-05-28 21:00:00", table: 1},
-  {start_time: "2015-05-28 22:00:00", end_time: "2015-05-28 22:55:00", table: 1}
-])
-Reservation.create!([
+  {start_time: "2015-05-28 22:00:00", end_time: "2015-05-28 22:55:00", table: 1},
   {start_time: "2015-05-28 19:00:00", end_time: "2015-05-28 20:00:00", table: 2},
   {start_time: "2015-05-28 20:30:00", end_time: "2015-05-28 21:00:00", table: 2},
-  {start_time: "2015-05-28 22:00:00", end_time: "2015-05-28 22:55:00", table: 2}
-])
-Reservation.create!([
+  {start_time: "2015-05-28 22:00:00", end_time: "2015-05-28 22:55:00", table: 2},
   {start_time: "2015-05-28 19:00:00", end_time: "2015-05-28 20:00:00", table: 3},
   {start_time: "2015-05-28 20:30:00", end_time: "2015-05-28 21:00:00", table: 3},
-  {start_time: "2015-05-28 22:00:00", end_time: "2015-05-28 22:55:00", table: 3}
-])
-Reservation.create!([
+  {start_time: "2015-05-28 22:00:00", end_time: "2015-05-28 22:55:00", table: 3},
   {start_time: "2015-05-28 19:00:00", end_time: "2015-05-28 20:00:00", table: 4},
   {start_time: "2015-05-28 20:30:00", end_time: "2015-05-28 21:00:00", table: 4},
   {start_time: "2015-05-28 22:00:00", end_time: "2015-05-28 22:55:00", table: 4}
